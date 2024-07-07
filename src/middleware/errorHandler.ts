ca271d767e4904383524fab3e07a2c4ea6ae9ecd
Rawdney Mendes <mendesrawdney@gmail.com>
2024-07-07 12:22:50 -0300

    update activities, challenges, and apis

--- src/middleware/errorHandler.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err);
-  res.status(500).json({ message: err.message });
-};
-
-export default errorHandler;
